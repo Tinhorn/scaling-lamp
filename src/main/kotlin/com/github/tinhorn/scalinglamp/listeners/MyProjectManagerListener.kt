81cb2f743b8e66fb634e734212d7caaa72d231af
GitHub Action <action@github.com>
2022-09-08 00:07:29 +0000

    Template cleanup

--- src/main/kotlin/com/github/tinhorn/scalinglamp/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/tinhorn/scalinglamp/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.tinhorn.scalinglamp.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.tinhorn.scalinglamp.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/tinhorn/scalinglamp/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/tinhorn/scalinglamp/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.tinhorn.scalinglamp.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.tinhorn.scalinglamp.MyBundle
 
 class MyApplicationService {
 
